48b248a40de2bc6bb13b09cdcf26f7df18a7cc19
Yash vardhan <yashvardhan513@gmail.com>
2020-12-12 12:29:00 +0530

    added template

---
+++ app.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require('mongoose-findorcreate');
+
+
+const app = express();
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+
+app.listen(3000, function() {
+  console.log("Server has started successfully");
+});
