c79c1b6ccdce6198a841ee36844efedae0baf442
Hirdeshgupta <hirdeshgupta16@gmail.com>
2020-12-14 10:29:48 +0530

    Login Logout button added

--- app.js
+++ app.js
@@ -9,38 +9,29 @@ const passportLocalMongoose = require("passport-local-mongoose");
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const findOrCreate = require('mongoose-findorcreate');
 const request = require("request");
-
-
 const app = express();
-
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({
-  extended: true
+    extended: true
 }));
-
-mongoose.connect("mongodb+srv://admin-yash:Yash123@cluster0-1lje1.mongodb.net/userDB", {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect("mongodb+srv://admin-yash:Yash123@cluster0-1lje1.mongodb.net/userDB", { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.set('useCreateIndex', true);
-
-app.get("/", function(req, res){
-  res.render("home");
+app.get("/", function(req, res) {
+    res.render("home");
 });
-
 let hospital = [];
 let hospital_array = [];
-request("http://www.communitybenefitinsight.org/api/get_hospitals.php?state=NC", function(error, response, body){
-  hospital = JSON.parse(body);
-  hospital.slice(0, 100);
+request("http://www.communitybenefitinsight.org/api/get_hospitals.php?state=NC", function(error, response, body) {
+    hospital = JSON.parse(body);
+    hospital.slice(0, 100);
 });
 
-app.get("/hospital", function(req, res){
-  res.render("hospital", {
-    hospital_data: hospital,
-  });
+app.get("/hospital", function(req, res) {
+    res.render("hospital", {
+        hospital_data: hospital,
+    });
 });
-
-
-
 app.listen(3000, function() {
-  console.log("Server has started successfully");
+    console.log("Server has started successfully");
 });
---
+++ dist/app.dev.js
@@ -0,0 +1,52 @@
+"use strict";
+
+require('dotenv').config();
+
+var express = require("express");
+
+var bodyParser = require("body-parser");
+
+var ejs = require("ejs");
+
+var mongoose = require("mongoose");
+
+var session = require("express-session");
+
+var passport = require("passport");
+
+var passportLocalMongoose = require("passport-local-mongoose");
+
+var GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+var findOrCreate = require('mongoose-findorcreate');
+
+var request = require("request");
+
+var app = express();
+app.use(express["static"]("public"));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+mongoose.connect("mongodb+srv://admin-yash:Yash123@cluster0-1lje1.mongodb.net/userDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+mongoose.set('useCreateIndex', true);
+app.get("/", function (req, res) {
+  res.render("home");
+});
+var hospital = [];
+var hospital_array = [];
+request("http://www.communitybenefitinsight.org/api/get_hospitals.php?state=NC", function (error, response, body) {
+  hospital = JSON.parse(body);
+  hospital.slice(0, 100);
+});
+app.get("/hospital", function (req, res) {
+  res.render("hospital", {
+    hospital_data: hospital
+  });
+});
+app.listen(3000, function () {
+  console.log("Server has started successfully");
+});
