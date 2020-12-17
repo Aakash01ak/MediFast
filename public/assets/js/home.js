2295d4f03cf336b32078879014b845657a1cb31a
Aakash01ak <aksingh01ak@gmail.com>
2020-12-17 19:04:51 +0530

    add gallery

--- public/assets/js/home.js
+++ public/assets/js/home.js
@@ -130,3 +130,21 @@ jQuery(function ($) {
 	$this.countTo(options);
   }
 });
+
+
+// Gallery
+
+const panels = document.querySelectorAll('.panel');
+
+panels.forEach((panel) => {
+  panel.addEventListener('click', () => {
+    removeActiveClasses();
+    panel.classList.add('active');
+  })
+})
+
+function removeActiveClasses() {
+  panels.forEach(panel => {
+    panel.classList.remove('active');
+  })
+}
